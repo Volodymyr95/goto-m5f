a05c5d6ecb2029ceae3995eb8d48b4edaa27c8ac
Volodymyr” <krokhmalyuk777@gmail.com>
2024-10-10 19:55:24 +0300

    initial commit

---
+++ src/main/java/com/codegym/jrugotom5/JruGotoM5Application.java
@@ -0,0 +1,13 @@
+package com.codegym.jrugotom5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JruGotoM5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JruGotoM5Application.class, args);
+    }
+
+}
