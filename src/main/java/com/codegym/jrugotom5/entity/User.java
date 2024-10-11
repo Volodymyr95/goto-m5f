a00d324972a56d439f70a0b6a5622e0ae3cb11db
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-11 22:55:22 +0200

    refactor: change to correct column names

--- src/main/java/com/codegym/jrugotom5/entity/User.java
+++ src/main/java/com/codegym/jrugotom5/entity/User.java
@@ -1,9 +1,6 @@
 package com.codegym.jrugotom5.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -15,8 +12,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
     private Integer age;
