067c37a7ba9c4ba96aee5bba6da4a96ba46eab13
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-11 18:00:49 +0200

    feat: add user entity

--- src/main/java/com/codegym/jrugotom5/entity/User.java
+++ src/main/java/com/codegym/jrugotom5/entity/User.java
@@ -1,4 +1,25 @@
 package com.codegym.jrugotom5.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "id")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Integer age;
+
+    private String email;
 }
