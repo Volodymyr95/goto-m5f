baaad9636fa01ad6ecc5af802eb242c66c7bfea9
Olena Borodych <olenaborodych@gmail.com>
2024-10-14 18:51:39 +0300

    added controller, repository and service for GET Ads by date range

---
+++ src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
@@ -0,0 +1,30 @@
+package com.codegym.jrugotom5.controller;
+
+import com.codegym.jrugotom5.entity.Advert;
+import com.codegym.jrugotom5.service.AdvertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/adverts")
+public class AdvertController {
+    AdvertService advertService;
+    public AdvertController( @Autowired AdvertService advertService ) {
+        this.advertService = advertService;
+    }
+
+        @GetMapping(path = "/", produces = "application/json")
+        public List<Advert> getAdverts(@RequestParam Date from, Date to) {
+            return this.advertService.getAdvertsByDateRange(from, to);
+        }
+
+
+
+    }
+
--- src/main/java/com/codegym/jrugotom5/service/AdvertService.java
+++ src/main/java/com/codegym/jrugotom5/service/AdvertService.java
@@ -1,4 +1,24 @@
 package com.codegym.jrugotom5.service;
 
+import com.codegym.jrugotom5.entity.Advert;
+import com.codegym.jrugotom5.repository.AdvertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class AdvertService {
+
+        AdvertRepository advertRepository;
+        public AdvertService( @Autowired AdvertRepository advertRepository ) {
+            this.advertRepository = advertRepository;
+        }
+
+
+    public List<Advert> getAdvertsByDateRange(Date from, Date to) {
+
+            return this.advertRepository.findAllByCreatedDateBetween(from, to);
+    }
 }
