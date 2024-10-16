1858313b6a322a95c7ba0e2ce38824762d33c95a
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-17 02:20:49 +0300

    refactor: remove redundant override

--- src/main/java/com/codegym/jrugotom5/repository/UserRepository.java
+++ src/main/java/com/codegym/jrugotom5/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Override
     List<User> findAll();
 }
