a7f7c7a68483af2502ba9a98907969671f369025
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-16 08:27:47 +0300

    feat: add user dto

---
+++ src/main/java/com/codegym/jrugotom5/dto/UserInfoDTO.java
@@ -0,0 +1,12 @@
+package com.codegym.jrugotom5.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserInfoDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
