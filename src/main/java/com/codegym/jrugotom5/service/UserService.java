618ce326b9768e0f7b2859bfc54ad2847386659f
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-16 12:47:43 +0300

    fix: remove unnecessary validation

--- src/main/java/com/codegym/jrugotom5/service/UserService.java
+++ src/main/java/com/codegym/jrugotom5/service/UserService.java
@@ -2,7 +2,6 @@ package com.codegym.jrugotom5.service;
 
 import com.codegym.jrugotom5.dto.UserInfoDTO;
 import com.codegym.jrugotom5.entity.User;
-import com.codegym.jrugotom5.exception.UserFetchException;
 import com.codegym.jrugotom5.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +19,6 @@ public class UserService {
 
     public List<UserInfoDTO> getAllUsers() {
         List<User> allUsers = userRepository.findAll();
-        if (allUsers.isEmpty()) {
-            log.error("No Users found");
-            throw new UserFetchException("No Users found");
-        }
         return allUsers.stream()
                 .map(user -> modelMapper.map(user, UserInfoDTO.class))
                 .toList();
