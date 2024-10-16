d7c159416a5b898167525a7f2a110431f1e6344c
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-16 08:41:21 +0300

    test: add unit tests for service

---
+++ src/test/java/com/codegym/jrugotom5/service/UserServiceTest.java
@@ -0,0 +1,63 @@
+package com.codegym.jrugotom5.service;
+
+import com.codegym.jrugotom5.dto.UserInfoDTO;
+import com.codegym.jrugotom5.entity.User;
+import com.codegym.jrugotom5.exception.UserFetchException;
+import com.codegym.jrugotom5.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAll_OneUser() {
+        User user = new User();
+        List<User> oneUser = List.of(user);
+
+        when(userRepository.findAll()).thenReturn(oneUser);
+        when(modelMapper.map(user, UserInfoDTO.class)).thenReturn(new UserInfoDTO());
+
+        List<UserInfoDTO> usersFromService = userService.getAllUsers();
+
+        assertEquals(1, usersFromService.size());
+
+        verify(userRepository, times(1)).findAll();
+        verify(modelMapper, times(1)).map(user, UserInfoDTO.class);
+    }
+
+    @Test
+    public void testGetAll_NoUsers() {
+        ArrayList<User> emptyUsers = new ArrayList<>();
+        when(userRepository.findAll()).thenReturn(emptyUsers);
+
+        assertThrows(UserFetchException.class,
+                () -> userService.getAllUsers());
+
+        verify(userRepository, times(1)).findAll();
+        verify(modelMapper, never()).map(any(), eq(UserInfoDTO.class));
+    }
+}
