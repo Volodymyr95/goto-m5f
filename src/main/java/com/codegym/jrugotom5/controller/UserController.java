d9394be0e352453390a03618e68fd9dcc9fbf5c8
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-17 18:37:10 +0300

    refactor: rename method and class

--- src/main/java/com/codegym/jrugotom5/controller/UserController.java
+++ src/main/java/com/codegym/jrugotom5/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.codegym.jrugotom5.controller;
 
-import com.codegym.jrugotom5.dto.UserNameDTO;
+import com.codegym.jrugotom5.dto.UserBasicInfoDTO;
 import com.codegym.jrugotom5.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserNameDTO> getUsers() {
+    public List<UserBasicInfoDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 }
