58ffa2cb4c0f3c5495a925d83b3714d11645fe84
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-10-17 21:40:51 +0200

    feat: add advert controller

--- src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
+++ src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
@@ -1,8 +1,8 @@
 package com.codegym.jrugotom5.controller;
 
-import com.codegym.jrugotom5.entity.Advert;
+import com.codegym.jrugotom5.dto.AdvertInfoForCreatorDto;
 import com.codegym.jrugotom5.service.AdvertService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/api/ads")
 public class AdvertController {
     private final AdvertService advertService;
 
     @GetMapping("/{userId}")
-    public List<Advert> getAdvertsByUser (@PathVariable int userId) {
-        return advertService.getAdvertsByUser(userId);
+    public List<AdvertInfoForCreatorDto> getAdvertsByUser(@PathVariable Long userId) {
+        return advertService.getAdvertsByCreateBy(userId);
     }
 }
