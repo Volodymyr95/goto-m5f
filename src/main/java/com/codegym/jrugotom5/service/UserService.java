d7d5451018fe89dabad0278d61537926a7925be4
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-17 20:18:47 +0300

    test: type cast and stream instead of forEach

--- src/main/java/com/codegym/jrugotom5/service/UserService.java
+++ src/main/java/com/codegym/jrugotom5/service/UserService.java
@@ -1,13 +1,13 @@
 package com.codegym.jrugotom5.service;
 
 import com.codegym.jrugotom5.dto.UserBasicInfoDTO;
+import com.codegym.jrugotom5.entity.User;
 import com.codegym.jrugotom5.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,9 +18,9 @@ public class UserService {
     private final ModelMapper modelMapper;
 
     public List<UserBasicInfoDTO> getAllUsers() {
-        List<UserBasicInfoDTO> usersDto = new ArrayList<>();
-        userRepository.findAll().forEach(user ->
-                usersDto.add(modelMapper.map(user, UserBasicInfoDTO.class)));
-        return usersDto;
+        List<User> allUsers = (List<User>) userRepository.findAll();
+        return allUsers.stream()
+                .map(user -> modelMapper.map(user, UserBasicInfoDTO.class))
+                .toList();
     }
 }
