76b4a47f8c8e5b75102d0fc504777709832f81d5
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-17 07:36:25 +0300

    feat: add advert service

--- src/main/java/com/codegym/jrugotom5/service/AdvertService.java
+++ src/main/java/com/codegym/jrugotom5/service/AdvertService.java
@@ -1,4 +1,24 @@
 package com.codegym.jrugotom5.service;
 
+import com.codegym.jrugotom5.dto.AdvertDTO;
+import com.codegym.jrugotom5.repository.AdvertRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class AdvertService {
+    private final AdvertRepository advertRepository;
+    private final ModelMapper modelMapper;
+
+    public List<AdvertDTO> getAllAdverts() {
+        return advertRepository.findAll().stream()
+                .map(advert->modelMapper.map(advert, AdvertDTO.class))
+                .toList();
+    }
 }
