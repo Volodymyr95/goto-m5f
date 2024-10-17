3f128ec9c18947c80c7b44b7eee7bf22571fa964
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-10-17 21:30:15 +0200

    feat: add app configuration

---
+++ src/main/java/com/codegym/jrugotom5/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.codegym.jrugotom5.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
