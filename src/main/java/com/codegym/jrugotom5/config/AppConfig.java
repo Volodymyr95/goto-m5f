d47c2b071c17c50baff4fcaef0062d7cba63aff4
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-10-21 20:17:21 +0300

    refactor: remove local variable

--- src/main/java/com/codegym/jrugotom5/config/AppConfig.java
+++ src/main/java/com/codegym/jrugotom5/config/AppConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        return modelMapper;
+        return new ModelMapper();
     }
 }
