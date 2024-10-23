992c609304bdb3bc74b2d13b40d36ac06dd69db3
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-10-23 23:16:29 +0200

    feat: add log to exception

--- src/main/java/com/codegym/jrugotom5/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codegym/jrugotom5/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.codegym.jrugotom5.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidDateRangeException.class)
@@ -14,6 +16,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidUserIdException.class)
     public ResponseEntity<String> handleInvalidUserId(InvalidUserIdException ex) {
+        log.error("User id is less than 1");
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
