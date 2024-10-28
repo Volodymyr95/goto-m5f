69a0efe6c18d6c0501cb33923992e7a1a9c2a76b
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-28 16:10:50 +0100

    refactor: remove unused imports

--- src/main/java/com/codegym/jrugotom5/JruGotoM5Application.java
+++ src/main/java/com/codegym/jrugotom5/JruGotoM5Application.java
@@ -1,9 +1,7 @@
 package com.codegym.jrugotom5;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JruGotoM5Application {
