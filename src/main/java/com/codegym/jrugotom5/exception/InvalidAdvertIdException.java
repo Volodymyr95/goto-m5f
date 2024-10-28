45258b2e1927e24623ef8869cd10f0a063eedcf7
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-28 16:08:38 +0100

    feat: add method to the service and add custom exceptions

---
+++ src/main/java/com/codegym/jrugotom5/exception/InvalidAdvertIdException.java
@@ -0,0 +1,7 @@
+package com.codegym.jrugotom5.exception;
+
+public class InvalidAdvertIdException extends RuntimeException {
+    public InvalidAdvertIdException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/codegym/jrugotom5/service/AdvertService.java
+++ src/main/java/com/codegym/jrugotom5/service/AdvertService.java
@@ -1,18 +1,20 @@
 package com.codegym.jrugotom5.service;
 
+import com.codegym.jrugotom5.dto.AdvertBasicInfoDTO;
 import com.codegym.jrugotom5.dto.AdvertFullInfoDTO;
 import com.codegym.jrugotom5.entity.Advert;
-import com.codegym.jrugotom5.repository.AdvertRepository;
+import com.codegym.jrugotom5.exception.EntityNotFoundException;
+import com.codegym.jrugotom5.exception.InvalidAdvertIdException;
 import com.codegym.jrugotom5.exception.InvalidDateRangeException;
+import com.codegym.jrugotom5.repository.AdvertRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
-import com.codegym.jrugotom5.dto.AdvertBasicInfoDTO;
-import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,7 +43,24 @@ public class AdvertService {
 
     public List<AdvertBasicInfoDTO> getAllAdverts() {
         return Streamable.of(advertRepository.findAll())
-                .map(advert->modelMapper.map(advert, AdvertBasicInfoDTO.class))
+                .map(advert -> modelMapper.map(advert, AdvertBasicInfoDTO.class))
                 .toList();
+    }
+
+    public AdvertFullInfoDTO getAdvertById(Long id) {
+        if (id <= 0) {
+            log.error("Invalid advert id: {}", id);
+            throw new InvalidAdvertIdException("Advert id must be a positive number.");
+        }
+
+        Advert advert = advertRepository.findAdvertById(id);
+        if (advert == null) {
+            log.error("Advert with id {} not found.", id);
+            throw new EntityNotFoundException("Advert with id " + id + " not found.");
+        }
+
+        AdvertFullInfoDTO dto = modelMapper.map(advert, AdvertFullInfoDTO.class);
+        dto.setUserCreatorId(advert.getCreatedBy().getId());
+        return dto;
     }
 }
