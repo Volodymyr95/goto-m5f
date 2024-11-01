84a243bc5fef1841563dde12eca0913ee09ea28a
Oleksandr Osadchuk <sania.elder@gmail.com>
2024-11-01 09:42:20 +0200

    build: add swagger dependency

--- src/main/resources/db/migrations/V3__add_sample_data.sql
+++ src/main/resources/db/migrations/V3__add_sample_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO user (age, email, first_name, last_name)
+INSERT INTO `user` (age, email, first_name, last_name)
 VALUES (25, 'john.doe@example.com', 'John', 'Doe'),
        (30, 'jane.smith@example.com', 'Jane', 'Smith'),
        (28, 'michael.brown@example.com', 'Michael', 'Brown'),
        (35, 'emily.jones@example.com', 'Emily', 'Jones'),
        (22, 'robert.johnson@example.com', 'Robert', 'Johnson');
 
-INSERT INTO advert (created_date, description, end_date, is_active, title, user_id, category)
+INSERT INTO `advert` (created_date, description, end_date, is_active, title, user_id, category)
 VALUES ('2024-10-22', 'Spacious apartment for rent', '2024-11-21', 1, 'Apartment for Rent', 1, 'REAL_ESTATE'),
        ('2024-10-05', 'Selling a used car in great condition', '2024-11-04', 1, 'Used Car for Sale', 1, 'VEHICLES'),
        ('2024-09-25', 'Job opening for software developer', '2024-10-25', 1, 'Software Developer Job', 2, 'JOBS'),
