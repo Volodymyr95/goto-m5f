f20185c4cb44d16acba73fd29c63245656be2668
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-11-02 21:33:07 +0100

    feat: add exception handler

--- src/main/java/com/codegym/jrugotom5/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codegym/jrugotom5/exception/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package com.codegym.jrugotom5.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
@@ -17,9 +22,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(InvalidUserIdException.class)
-    public ResponseEntity<String> handleInvalidUserId(InvalidUserIdException ex) {
-        log.error("An error occurred: ", ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleInvalidUserId(InvalidUserIdException e) {
+        log.error("An error occurred: {}", e.getMessage(), e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
@@ -32,5 +37,24 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleInvalidCategoryException(InvalidCategoryException e) {
         log.error("Occurred Exception: {} ", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Map<String, String>> handleMethodValidationException(HandlerMethodValidationException e) {
+        log.error("Occurred Exception: {} ", e.getMessage(), e);
+        HashMap<String, String> errors = new HashMap<>();
+
+        e.getAllErrors().forEach(
+                error -> {
+                    if (error instanceof FieldError) {
+                        String fieldName = ((FieldError) error).getField();
+                        String errorMessage = error.getDefaultMessage();
+                        errors.put(fieldName, errorMessage);
+                    } else {
+                        errors.put("error", error.getDefaultMessage());
+                    }
+                }
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
