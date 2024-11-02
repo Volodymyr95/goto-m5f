e7bd235b723e448cfbc2211d10953443a12e5c51
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-11-02 16:06:00 +0100

    refactor: remove unused custom exception

--- src/main/java/com/codegym/jrugotom5/exception/InvalidAdvertIdException.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.jrugotom5.exception;
-
-public class InvalidAdvertIdException extends RuntimeException {
-    public InvalidAdvertIdException(String message) {
-        super(message);
-    }
-}
