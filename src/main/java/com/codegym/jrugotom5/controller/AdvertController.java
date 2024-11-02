66f4431785974bc6237a7ccf1c716729bdc0a5c4
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-11-02 16:09:16 +0100

    feat: add swagger open API to the endpoint

--- src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
+++ src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
@@ -3,16 +3,20 @@ package com.codegym.jrugotom5.controller;
 import com.codegym.jrugotom5.dto.AdvertBasicInfoDTO;
 import com.codegym.jrugotom5.dto.AdvertFullInfoDTO;
 import com.codegym.jrugotom5.service.AdvertService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "Advert Controller", description = "Advert APIs")
 @RestController
 @RequestMapping("/api/adverts")
 @RequiredArgsConstructor
@@ -37,11 +41,24 @@ public class AdvertController {
     @GetMapping("/{category}/")
     public List<AdvertBasicInfoDTO> getByCategory(@PathVariable String category) {
         return advertService.getByCategory(category);
-
     }
 
+    @Operation(
+            summary = "Retrieve an Advert by Id",
+            description = """
+                     Get an Advert object by specifying its id.\s
+                     The response is Advert object with id, title, description, user creator id, created date,\s
+                     end date and active status.
+                    \s""",
+            tags = {"adverts", "get"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = AdvertFullInfoDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema())}, description = "Receive a 400 error if id is less than 1"),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())}, description = "Receive a 404 error if the Advert is not found by this id")
+    })
     @GetMapping("/{id}")
-    public AdvertFullInfoDTO getAdvertById(@PathVariable Long id) {
+    public AdvertFullInfoDTO getAdvertById(@PathVariable @Min(value = 1,
+            message = "Advert id must be greater than 0") Long id) {
         return advertService.getAdvertById(id);
     }
 }
