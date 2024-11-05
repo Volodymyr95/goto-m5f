7af9901c2f49f60da2476945febe9b2190f777ad
Oleksandr Osadchuk <sania.elder@gmail.com>
2024-11-06 01:00:07 +0200

    docs: add Swagger OAS annotations

--- src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
+++ src/main/java/com/codegym/jrugotom5/controller/AdvertController.java
@@ -3,6 +3,11 @@ package com.codegym.jrugotom5.controller;
 import com.codegym.jrugotom5.dto.AdvertBasicInfoDTO;
 import com.codegym.jrugotom5.dto.AdvertFullInfoDTO;
 import com.codegym.jrugotom5.service.AdvertService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +34,28 @@ public class AdvertController {
         return advertService.getAdvertsByDateRange(from, to);
     }
 
+    @Operation(summary = "Retrieve an Advert by phrase contains in title", tags = {"adverts", "get"})
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = AdvertBasicInfoDTO.class), mediaType = "application/json"))
     @GetMapping("/adverts")
     public List<AdvertBasicInfoDTO> getByTitleContains(@RequestParam(value = "title") String phrase) {
         return advertService.getByTitleContains(phrase);
     }
 
-    @GetMapping("/{category}/")
+    @Operation(summary = "Retrieve an Advert by Id", tags = {"adverts", "get"})
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            schema = @Schema(implementation = AdvertBasicInfoDTO.class),
+                            mediaType = "application/json")),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema()),
+                    description = "Receive a 400 error if such Category doesn't exist")
+    })
+    @GetMapping("/{category}")
     public List<AdvertBasicInfoDTO> getByCategory(@PathVariable String category) {
         return advertService.getByCategory(category);
 
